a341ded9d8303d9726066f7d01f6dee58a3d5b42
PagalaRamudu <ramupagala55@example.com>
2023-10-25 15:18:22 +0530

    added comment

--- ecom-backend/src/main/java/com/ecom/Ecomm.java
+++ ecom-backend/src/main/java/com/ecom/Ecomm.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Ecomm {
-
+	//Main class 
     public static void main(String[] args) {
         SpringApplication.run(Ecomm.class, args);
     }
